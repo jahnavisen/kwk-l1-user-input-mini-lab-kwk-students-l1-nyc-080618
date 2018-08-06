a52d2675da93571abc8232c183db67672ec024ef
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-06 19:13:24 +0000

    automatically backed up by learn

--- user_input.rb
+++ user_input.rb
@@ -2,3 +2,6 @@ puts "whats your name?"
 name =gets.chomp 
 puts "my name is #{name} and your watching disney channel." 
 
+puts "whats your favorite dessert?"
+dessert =gets.chomp
+puts "my favorite dessert is #{dessert}"
